b479a68bff676552adc490cb0012b3ccd0f78b33
guozhanqiang <guozhanqiang@guozhanqiangdeMacBook-Pro.local>
2026-07-14 19:17:50 +0800

    new task.json
    Change-Id: I208ab896cc74f131b362593274ae6f56c99c7a0a

--- read_write_data.py
+++ read_write_data.py
@@ -15,5 +15,26 @@ def data_process():
     with open(json_name, "w", encoding="utf-8") as f:
         json.dump(obj, f, ensure_ascii=False, indent=4)
 
+def adjust_json():
+    json_name = "./all_task_old.json"
+    with open(json_name, "r", encoding="utf-8") as f:
+        data_old = json.load(f)
+    json_name = "./all_task_new.json"
+    with open(json_name, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    task_id_list = []
+    for item in data_old:
+        if item["task_id"] in task_id_list:
+            print("error")
+        task_id_list.append(item["task_id"])
+    data_dict = {}
+    for item in data:
+        data_dict[item["task_id"]] = item
+    new_data = []
+    for task_id in task_id_list:
+        new_data.append(data_dict[task_id])
+    with open("./all_task.json", "w", encoding="utf-8") as f:
+        json.dump(new_data, f, ensure_ascii=False, indent=4)
+
 if __name__ == "__main__":
     data_process()
